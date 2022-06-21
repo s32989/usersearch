409e079e22e331ff575ac52079aadbb2786b2455
s32989 <s32989@gmail.com>
2022-06-20 23:35:21 -0400

    restructure

---
+++ backend/src/test/java/com/preciselytakehome/usersearch/UsersearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.preciselytakehome.usersearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
